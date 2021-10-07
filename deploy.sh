bf83b04a0a60a0aeea19f8fb7b5d5d6f10b60cc1
songge <songgege@dingtalk.com>
2021-10-07 17:45:41 +0800

    delete file

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd storybook-static
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:northui/northui.github.io.git master
